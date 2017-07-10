7494e93b8f9efc9e3cd698df7278b79a54f50b35
Andrew McGhie <andrew.mcghie@gmail.com>
2017-07-10 18:06:05 +1000

    Issue #11: Changed the component-tests to use a virtual broker

--- core/component-test/src/test/java/uk/gov/dwp/queue/triage/core/health/FailedMessageListenerComponentTest.java
+++ core/component-test/src/test/java/uk/gov/dwp/queue/triage/core/health/FailedMessageListenerComponentTest.java
@@ -1,18 +1,12 @@
 package uk.gov.dwp.queue.triage.core.health;
 
 import com.tngtech.jgiven.annotation.ScenarioStage;
-import com.tngtech.jgiven.integration.spring.EnableJGiven;
-import com.tngtech.jgiven.integration.spring.SimpleSpringRuleScenarioTest;
 import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.dwp.queue.triage.core.BaseCoreComponentTest;
 import uk.gov.dwp.queue.triage.core.FailedMessageResourceStage;
 import uk.gov.dwp.queue.triage.core.JmsStage;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
-@EnableJGiven
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-public class FailedMessageListenerComponentTest extends SimpleSpringRuleScenarioTest<JmsStage> {
+public class FailedMessageListenerComponentTest extends BaseCoreComponentTest<JmsStage> {
 
     @ScenarioStage
     private FailedMessageResourceStage failedMessageResourceStage;
