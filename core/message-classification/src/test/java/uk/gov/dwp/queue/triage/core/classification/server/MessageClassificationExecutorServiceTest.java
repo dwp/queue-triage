6ca7a76d34746de3e9f9fc5e0709c7ce1cb512d7
Andrew McGhie <andrew.mcghie@gmail.com>
2017-08-24 21:27:58 +1000

    #20: Attempting to fix flickery MessageClassificationExecutorServiceTest

--- core/message-classification/src/test/java/uk/gov/dwp/queue/triage/core/classification/server/MessageClassificationExecutorServiceTest.java
+++ core/message-classification/src/test/java/uk/gov/dwp/queue/triage/core/classification/server/MessageClassificationExecutorServiceTest.java
@@ -1,14 +1,13 @@
 package uk.gov.dwp.queue.triage.core.classification.server;
 
 import org.junit.Test;
-import org.mockito.Mockito;
-import org.mockito.verification.Timeout;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
 public class MessageClassificationExecutorServiceTest {
@@ -19,14 +18,14 @@ public class MessageClassificationExecutorServiceTest {
             Executors.newSingleThreadScheduledExecutor(),
             messageClassificationService,
             0,
-            50,
+            100,
             TimeUnit.MILLISECONDS
     );
 
     @Test
     public void jobExecutesSuccessfully() throws Exception {
         underTest.start();
-        verify(messageClassificationService, times(1, timeout(50))).classifyFailedMessages();
+        verify(messageClassificationService, timeout(75).times(1)).classifyFailedMessages();
         underTest.stop();
     }
 
@@ -36,7 +35,7 @@ public class MessageClassificationExecutorServiceTest {
 
         underTest.start();
 
-        verify(messageClassificationService, times(2, timeout(80))).classifyFailedMessages();
+        verify(messageClassificationService, timeout(175).times(2)).classifyFailedMessages();
 
         underTest.stop();
     }
@@ -52,28 +51,20 @@ public class MessageClassificationExecutorServiceTest {
         );
         underTest.start();
 
-        verify(messageClassificationService, times(0, timeout(50))).classifyFailedMessages();
+        verify(messageClassificationService, timeout(50).times(0)).classifyFailedMessages();
         underTest.execute();
-        verify(messageClassificationService, times(1, timeout(50))).classifyFailedMessages();
+        verify(messageClassificationService, timeout(75).times(1)).classifyFailedMessages();
         underTest.stop();
     }
 
     @Test
     public void executorCanBePausedAndResumed() {
         underTest.start();
-        verify(messageClassificationService, times(1, timeout(50))).classifyFailedMessages();
+        verify(messageClassificationService, timeout(75).times(1)).classifyFailedMessages();
         underTest.pause();
-        verify(messageClassificationService, times(1, timeout(75))).classifyFailedMessages();
+        verify(messageClassificationService, timeout(75).times(1)).classifyFailedMessages();
         underTest.start();
-        verify(messageClassificationService, times(2, timeout(25))).classifyFailedMessages();
+        verify(messageClassificationService, timeout(75).times(2)).classifyFailedMessages();
         underTest.stop();
-    }
-
-    private Timeout times(int executions, long millis) {
-        return new Timeout(millis, Mockito.times(executions));
-    }
-
-    private long timeout(long millis) {
-        return millis;
     }
 }
