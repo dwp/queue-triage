0f54b7e26b1e2038d3414bb4fd3e6853e7f15e1d
Andrew McGhie <andrew.mcghie@gmail.com>
2017-09-25 19:14:12 +1000

    #68: Added some logging during testing and updated 'awaitility' polling interval for core component tests

--- core/component-test/src/main/java/uk/gov/dwp/queue/triage/core/search/SearchFailedMessageStage.java
+++ core/component-test/src/main/java/uk/gov/dwp/queue/triage/core/search/SearchFailedMessageStage.java
@@ -1,17 +1,22 @@
 package uk.gov.dwp.queue.triage.core.search;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.Format;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 import org.hamcrest.Matcher;
 import org.hamcrest.collection.IsEmptyIterable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import uk.gov.dwp.queue.triage.core.client.search.SearchFailedMessageRequest;
 import uk.gov.dwp.queue.triage.core.client.search.SearchFailedMessageRequest.SearchFailedMessageRequestBuilder;
 import uk.gov.dwp.queue.triage.core.client.search.SearchFailedMessageResponse;
 import uk.gov.dwp.queue.triage.jgiven.ReflectionArgumentFormatter;
@@ -19,7 +24,7 @@ import uk.gov.dwp.queue.triage.jgiven.ReflectionArgumentFormatter;
 import javax.ws.rs.core.Response.Status;
 import java.util.Collection;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.awaitility.Awaitility.await;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -27,8 +32,12 @@ import static org.hamcrest.Matchers.is;
 @JGivenStage
 public class SearchFailedMessageStage extends Stage<SearchFailedMessageStage> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SearchFailedMessageStage.class);
+
     @Autowired
     private TestRestTemplate testRestTemplate;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @ExpectedScenarioState
     private ResponseEntity<Collection<SearchFailedMessageResponse>> searchResponse;
@@ -52,14 +61,21 @@ public class SearchFailedMessageStage extends Stage<SearchFailedMessageStage> {
             @Format(value = ReflectionArgumentFormatter.class, args = {"broker", "destination"}) SearchFailedMessageRequestBuilder requestBuilder,
             Matcher<Iterable<? extends SearchFailedMessageResponse>> resultsMatcher
     ) {
-        await().atMost(5, SECONDS)
-                .until(testRestTemplate.exchange(
-                                "/core/failed-message/search",
-                                HttpMethod.POST,
-                                new HttpEntity<>(requestBuilder.build()),
-                                new ParameterizedTypeReference<Collection<SearchFailedMessageResponse>>() {})::getBody,
-                       resultsMatcher);
+        await().pollInterval(100, MILLISECONDS)
+                .until(() -> doSearch(requestBuilder), resultsMatcher);
         return this;
+    }
+
+    public Collection<SearchFailedMessageResponse> doSearch(SearchFailedMessageRequestBuilder requestBuilder) throws JsonProcessingException {
+        SearchFailedMessageRequest searchRequest = requestBuilder.build();
+        LOGGER.info("Executing Search: {}", objectMapper.writeValueAsString(searchRequest));
+        return testRestTemplate
+                .exchange(
+                        "/core/failed-message/search",
+                        HttpMethod.POST,
+                        new HttpEntity<>(searchRequest),
+                        new ParameterizedTypeReference<Collection<SearchFailedMessageResponse>>() {})
+                .getBody();
     }
 
     public SearchFailedMessageStage theSearchResultsContain(Matcher<Iterable<? extends SearchFailedMessageResponse>> resultsMatcher) {
