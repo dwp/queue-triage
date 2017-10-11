aaf6622534484bc101824b5383593664202e8e66
Andrew McGhie <andrew.mcghie@gmail.com>
2017-10-11 23:07:35 +1000

    #81: Added some additional logging to the FailedMessageChangeResource

--- web/server/src/main/java/uk/gov/dwp/queue/triage/web/server/api/FailedMessageChangeResource.java
+++ web/server/src/main/java/uk/gov/dwp/queue/triage/web/server/api/FailedMessageChangeResource.java
@@ -1,5 +1,7 @@
 package uk.gov.dwp.queue.triage.web.server.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.gov.dwp.queue.triage.core.client.delete.DeleteFailedMessageClient;
 import uk.gov.dwp.queue.triage.core.client.label.LabelFailedMessageClient;
 
@@ -16,6 +18,8 @@ import static uk.gov.dwp.queue.triage.id.FailedMessageId.fromString;
 @Produces(APPLICATION_JSON)
 public class FailedMessageChangeResource {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FailedMessageChangeResource.class);
+
     private final LabelFailedMessageClient labelFailedMessageClient;
     private final LabelExtractor labelExtractor;
     private final DeleteFailedMessageClient deleteFailedMessageClient;
@@ -31,10 +35,15 @@ public class FailedMessageChangeResource {
     @POST
     @Path("/labels")
     public String updateLabelsOnFailedMessages(LabelRequest request) {
+        LOGGER.info("Updating the labels on {} failedMessages", request.getChanges().size());
         request.getChanges()
-                .forEach(change -> labelFailedMessageClient.setLabels(
-                        fromString(change.getRecid()),
-                        labelExtractor.extractLabels(change.getLabels())));
+                .forEach(change -> {
+                    LOGGER.info("Setting labels on FailedMessage: {} to '{}'", change.getRecid(), change.getLabels());
+                    labelFailedMessageClient.setLabels(
+                            fromString(change.getRecid()),
+                            labelExtractor.extractLabels(change.getLabels())
+                    );
+                });
         return "{ 'status': 'success' }";
     }
 
