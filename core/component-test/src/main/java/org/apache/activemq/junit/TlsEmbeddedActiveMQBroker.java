75142dfa892ab6e8e9cec2d4e0d42afa9eb75ed1
Andrew McGhie <andrew.mcghie@gmail.com>
2018-04-11 00:15:14 +1000

    #146: Configured the TLS Embedded Broker to use the vm and ssl protocol

--- core/component-test/src/main/java/org/apache/activemq/junit/TlsEmbeddedActiveMQBroker.java
+++ core/component-test/src/main/java/org/apache/activemq/junit/TlsEmbeddedActiveMQBroker.java
@@ -1,48 +1,41 @@
 package org.apache.activemq.junit;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.ActiveMQSslConnectionFactory;
-import org.apache.activemq.broker.BrokerService;
-import org.apache.activemq.broker.SslBrokerService;
 import org.apache.activemq.broker.SslContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.ResourceUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.net.URI;
-import java.security.KeyStore;
-import java.security.SecureRandom;
-
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+import java.security.SecureRandom;
 
 public class TlsEmbeddedActiveMQBroker extends EmbeddedActiveMQBroker {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TlsEmbeddedActiveMQBroker.class);
-    private static final String BROKER_URL = "broker:(ssl://localhost:2023)";
+//    private static final String BROKER_URL = "broker:(ssl://localhost:2023)";
 
-    public TlsEmbeddedActiveMQBroker() {
-        super(BROKER_URL);
-    }
-
-        @Override
-    public ActiveMQConnectionFactory createConnectionFactory() {
-            String brokerUri = "ssl://localhost:2023";
-            ActiveMQSslConnectionFactory activeMQSslConnectionFactory = new ActiveMQSslConnectionFactory(brokerUri);
-        try {
-            LOGGER.info("Found TLS configuration for broker at: {}. Creating appropriate ssl connection factory.", brokerUri);
-
-            activeMQSslConnectionFactory.setKeyStore(ResourceUtils.getFile("classpath:./tls_activemq/client.ks").getPath());
-            activeMQSslConnectionFactory.setKeyStorePassword("clientpassword");
-            activeMQSslConnectionFactory.setTrustStore(ResourceUtils.getFile("classpath:./tls_activemq/broker.ks").getPath());
-            activeMQSslConnectionFactory.setTrustStorePassword("brokerpassword");
-            return activeMQSslConnectionFactory;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    public TlsEmbeddedActiveMQBroker() {
+//        super(BROKER_URL);
+//    }
+//
+//        @Override
+//    public ActiveMQConnectionFactory createConnectionFactory() {
+//            String brokerUri = "ssl://localhost:2023";
+//            ActiveMQSslConnectionFactory activeMQSslConnectionFactory = new ActiveMQSslConnectionFactory(brokerUri);
+//        try {
+//            LOGGER.info("Found TLS configuration for broker at: {}. Creating appropriate ssl connection factory.", brokerUri);
+//
+//            activeMQSslConnectionFactory.setKeyStore(ResourceUtils.getFile("classpath:./tls_activemq/client.ks").getPath());
+//            activeMQSslConnectionFactory.setKeyStorePassword("clientpassword");
+//            activeMQSslConnectionFactory.setTrustStore(ResourceUtils.getFile("classpath:./tls_activemq/client.ts").getPath());
+//            activeMQSslConnectionFactory.setTrustStorePassword("clientpassword");
+//            return activeMQSslConnectionFactory;
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     @Override
     protected void configure() {
@@ -50,43 +43,24 @@ public class TlsEmbeddedActiveMQBroker extends EmbeddedActiveMQBroker {
             LOGGER.info("Beginning to configure ActiveMQ embedded with TLS.");
             //loading keystore from file
             KeyStore keystore = KeyStore.getInstance("JKS");
+            keystore.load(new FileInputStream(ResourceUtils.getFile("classpath:./tls_activemq/broker.ks")), "brokerpassword".toCharArray());
 
-//            File ksfile = new File("/home/me/client1.pkcs12");
-            File ksfile = ResourceUtils.getFile("classpath:./tls_activemq/broker.ks");
-            FileInputStream ksfis = new FileInputStream(ksfile);
-            keystore.load(ksfis, "brokerpassword".toCharArray());
-
-//loading truststore from file
             KeyStore truststore = KeyStore.getInstance("JKS");
-//            File tsFile = new File("/home/me/client1.truststore");
-            File tsFile = ResourceUtils.getFile("classpath:./tls_activemq/client.ks");
-            truststore.load(new FileInputStream(tsFile), "clientpassword"
-                .toCharArray());
+            truststore.load(new FileInputStream(ResourceUtils.getFile("classpath:./tls_activemq/broker.ts")), "brokerpassword".toCharArray());
 
-            KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory
-                                                                      .getDefaultAlgorithm());
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
             kmf.init(keystore, "brokerpassword".toCharArray());
 
-            TrustManagerFactory tmf = TrustManagerFactory
-                .getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
             tmf.init(truststore);
 
-//broker definition
-//        String cfURI = "ssl://localhost:2032";
-//        BrokerService brokerService = new BrokerService();
-//        brokerService.addConnector(cfURI);
-
-//configure ssl context for the broker
-//            SslContext sslContext = new SslContext(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
-
-//need client authentication
             SslContext sslContext = new SslContext(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
             brokerService.setSslContext(sslContext);
+            brokerService.addConnector("ssl://localhost:2023");
+
 //            sslContext.getSSLContext().getDefaultSSLParameters().setNeedClientAuth(true);
 //            sslContext.getSSLContext().getDefaultSSLParameters().setWantClientAuth(true);
-//            brokerService.addConnector(new URI("ssl://localhost:2023"));
             LOGGER.info("Broker Supported Suites: {}", sslContext.getSSLContext().getDefaultSSLParameters().getAlgorithmConstraints());
-
         } catch (Exception e) {
             LOGGER.error("An error occurred while attempting to start the active MQ broker in TLS mode.", e);
         }
