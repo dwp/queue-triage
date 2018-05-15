a45b695faf710f94ccbd050a7b0170f5afa7095c
Andrew McGhie <andrew.mcghie@gmail.com>
2018-05-15 19:45:18 +1000

    #152: Implement the ResendFailedMessageAction

--- core/message-classification/src/main/java/uk/gov/dwp/queue/triage/core/classification/action/ResendFailedMessageAction.java
+++ core/message-classification/src/main/java/uk/gov/dwp/queue/triage/core/classification/action/ResendFailedMessageAction.java
@@ -1,13 +1,40 @@
 package uk.gov.dwp.queue.triage.core.classification.action;
 
+import com.fasterxml.jackson.annotation.JacksonInject;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import uk.gov.dwp.queue.triage.core.domain.FailedMessage;
+import uk.gov.dwp.queue.triage.core.domain.update.StatusUpdateRequest;
+import uk.gov.dwp.queue.triage.core.service.FailedMessageService;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.util.Optional;
+
+import static uk.gov.dwp.queue.triage.core.domain.StatusHistoryEvent.Status.RESEND;
 
 public class ResendFailedMessageAction implements FailedMessageAction {
 
+    private final Duration resendDelay;
+    private final FailedMessageService failedMessageService;
 
+    public ResendFailedMessageAction(@JsonProperty("resendDelay") Duration resendDelay,
+                                     @JacksonInject FailedMessageService failedMessageService) {
+        this.resendDelay = resendDelay;
+        this.failedMessageService = failedMessageService;
+    }
 
     @Override
     public void accept(FailedMessage failedMessage) {
+        failedMessageService.update(
+                failedMessage.getFailedMessageId(),
+                new StatusUpdateRequest(RESEND, getClock().instant().plus(getResendDelay())));
+    }
 
+    private Duration getResendDelay() {
+        return Optional.ofNullable(resendDelay).orElse(Duration.ofSeconds(0));
+    }
+
+    protected Clock getClock() {
+        return Clock.systemUTC();
     }
 }
---
+++ core/message-classification/src/test/java/uk/gov/dwp/queue/triage/core/classification/action/ResendFailedMessageActionTest.java
@@ -0,0 +1,92 @@
+package uk.gov.dwp.queue.triage.core.classification.action;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import uk.gov.dwp.queue.triage.core.domain.FailedMessage;
+import uk.gov.dwp.queue.triage.core.domain.update.StatusUpdateRequest;
+import uk.gov.dwp.queue.triage.core.service.FailedMessageService;
+import uk.gov.dwp.queue.triage.id.FailedMessageId;
+import uk.gov.dwp.queue.triage.jackson.configuration.JacksonConfiguration;
+
+import java.io.IOException;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneOffset;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.dwp.queue.triage.core.domain.StatusHistoryEvent.Status.RESEND;
+import static uk.gov.dwp.queue.triage.core.domain.update.StatusUpdateRequestMatcher.aStatusUpdateRequest;
+import static uk.gov.dwp.queue.triage.matchers.ReflectionEqualsMatcher.reflectionEquals;
+
+public class ResendFailedMessageActionTest {
+
+    private static final Duration TEN_SECONDS = Duration.ofSeconds(10);
+    private static final Clock FIXED_CLOCK = Clock.fixed(Instant.now(), ZoneOffset.UTC);
+    private static final FailedMessageId FAILED_MESSAGE_ID = FailedMessageId.newFailedMessageId();
+
+    private final FailedMessageService failedMessageService = mock(FailedMessageService.class);
+    private final FailedMessage failedMessage = mock(FailedMessage.class);
+    private final ArgumentCaptor<StatusUpdateRequest> statusUpdateRequest = ArgumentCaptor.forClass(StatusUpdateRequest.class);
+
+    private ObjectMapper objectMapper;
+
+
+    @Before
+    public void setUp() {
+        final JacksonConfiguration jacksonConfiguration = new JacksonConfiguration();
+        objectMapper = jacksonConfiguration.objectMapper().setInjectableValues(
+                jacksonConfiguration.jacksonInjectableValues().addValue(FailedMessageService.class, failedMessageService)
+        );
+        when(failedMessage.getFailedMessageId()).thenReturn(FAILED_MESSAGE_ID);
+    }
+
+    @Test
+    public void jsonCanBeDeserialised() throws IOException {
+        final FailedMessageAction failedMessageAction = objectMapper.readValue("{ \"_action\": \"resend\", \"resendDelay\": \"PT10S\" }", FailedMessageAction.class);
+
+        assertThat(failedMessageAction, reflectionEquals(new ResendFailedMessageAction(TEN_SECONDS, failedMessageService)));
+    }
+
+    @Test
+    public void jsonCanBeDeserialisedWithNullResendDelay() throws IOException {
+        final FailedMessageAction failedMessageAction = objectMapper.readValue("{ \"_action\": \"resend\", \"resendDelay\": null }", FailedMessageAction.class);
+
+        assertThat(failedMessageAction, reflectionEquals(new ResendFailedMessageAction(null, failedMessageService)));
+    }
+
+    @Test
+    public void resendFailedMessageActionWithNullDelay() {
+        ResendFailedMessageAction underTest = createResendFailedMessageAction(null);
+
+        underTest.accept(failedMessage);
+
+        verify(failedMessageService).update(eq(FAILED_MESSAGE_ID), statusUpdateRequest.capture());
+        assertThat(statusUpdateRequest.getValue(), aStatusUpdateRequest(RESEND).withUpdatedDateTime(FIXED_CLOCK.instant()));
+    }
+
+    @Test
+    public void resendFailedMessageActionWithGivenDelay() {
+        ResendFailedMessageAction underTest = createResendFailedMessageAction(Duration.ofSeconds(10));
+
+        underTest.accept(failedMessage);
+
+        verify(failedMessageService).update(eq(FAILED_MESSAGE_ID), statusUpdateRequest.capture());
+        assertThat(statusUpdateRequest.getValue(), aStatusUpdateRequest(RESEND).withUpdatedDateTime(FIXED_CLOCK.instant().plusSeconds(10)));
+    }
+
+    private ResendFailedMessageAction createResendFailedMessageAction(final Duration resendDelay) {
+        return new ResendFailedMessageAction(resendDelay, failedMessageService) {
+            @Override
+            protected Clock getClock() {
+                return FIXED_CLOCK;
+            }
+        };
+    }
+}
